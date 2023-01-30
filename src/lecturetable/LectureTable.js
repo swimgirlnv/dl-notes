fa484d6e6d7a60f7b9fcea41c38d8cd0e3e7a1c3
Becca <rebecca_waterson@brown.edu>
2023-01-30 10:55:52 -0500

    switching to chakra

--- src/lecturetable/LectureTable.js
+++ src/lecturetable/LectureTable.js
@@ -1,23 +1,47 @@
 import "./LectureTable.css"
 
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
+
 export default function LectureTable() {
 
   return (
     <div>
-      <table className="table">
-        <tr className="tr">
-          <th className="th">Date</th>
-          <th className="th">Lecture</th>
-          <th className="th">Misc</th>
-        </tr>
-        {/* Add on to this as more lectures/notes */}
-        <tr>
-          <td className="td">1/27/2023</td>   
-          <td className="td"><a href="#l1">Learn Basic Concepts of Machine Learning</a></td>
-          <td className="td"><a target="blank" href="https://drive.google.com/file/d/1dCS9DuzZVxGCTjybaU_xiWmeFnGU4mVB/view?usp=sharing">Slides</a></td>
-        </tr>
-
-      </table>
+      <TableContainer>
+        <Table className="table" variant='simple'>
+          <Thead>
+            <Tr className="tr">
+              <Th className="th">Date</Th>
+              <Th className="th">Lecture</Th>
+              <Th className="th">Misc</Th>
+            </Tr>
+          </Thead>
+          {/* Add on to this as more lectures/notes */}
+          <Tbody>
+            <Tr>
+              <Td className="td">1/27/2023</Td>   
+              <Td className="td"><a href="#l1">Learn Basic Concepts of Machine Learning</a></Td>
+              <Td className="td"><a target="blank" href="https://drive.google.com/file/d/1dCS9DuzZVxGCTjybaU_xiWmeFnGU4mVB/view?usp=sharing">Slides</a></Td>
+            </Tr>
+          </Tbody>
+          <Tfoot>
+            <Tr className="tr">
+              <Th className="th">Date</Th>
+              <Th className="th">Lecture</Th>
+              <Th className="th">Misc</Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </TableContainer>
     </div>
   )
 
--- src/notes/Lecture1.js
+++ src/notes/Lecture1.js
@@ -1,143 +1,190 @@
 import Return from "../buildingblocks/Return"
 import "./notes.css"
 
+import {
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+} from '@chakra-ui/react'
+
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
+
+import { Heading, Text, Stack } from '@chakra-ui/react'
+
+
 export default function lecture1 () {
 
   return (
     <div className="note">
       <div className="content">
         <div className="header">
-          <h2 className="title">1/27/23: Learn Basic Concepts of Machine Learning</h2>
-          <p>Slides can be found <a target="blank" href="https://drive.google.com/file/d/1dCS9DuzZVxGCTjybaU_xiWmeFnGU4mVB/view?usp=sharing">here</a></p>
+          <Heading className="title">1/27/23: Learn Basic Concepts of Machine Learning</Heading>
+          <Text>Slides can be found <a target="blank" href="https://drive.google.com/file/d/1dCS9DuzZVxGCTjybaU_xiWmeFnGU4mVB/view?usp=sharing">here</a></Text>
         </div>
-        <h3>How do we represent input/output?</h3>
-        <ul>
-          <li>Model == function</li>
-          <li>How can we represent input images as numbers?</li>
-          <ul>
-            <li>2D arrays of pixel types</li>
-          </ul>
-          <li>How can we represent output labels as numbers?</li>
-          <ul>
-            <li>Binary codings like 0 and 1</li>
-          </ul>
-          <li>"Binary classification":</li>
-          <ul>
-            <li>Input image X is X &#x2208; R<sup>H x W x 3</sup></li>
-            <li>Output Y is Y &#x2208; &#x7B; 0,1 &#x7D; ← categorical output</li>
-          </ul>
-          </ul>
+        <br></br>
+        <Stack spacing={1}>
+        <Text>How do we represent input/output?</Text>
+        <UnorderedList>
+          <ListItem>Model == function</ListItem>
+          <ListItem>How can we represent input images as numbers?</ListItem>
+          <UnorderedList>
+            <ListItem>2D arrays of pixel types</ListItem>
+          </UnorderedList>
+          <ListItem>How can we represent output labels as numbers?</ListItem>
+          <UnorderedList>
+            <ListItem>Binary codings ListItemke 0 and 1</ListItem>
+          </UnorderedList>
+          <ListItem>"Binary classification":</ListItem>
+          <UnorderedList>
+            <ListItem>Input image X is X &#x2208; R<sup>H x W x 3</sup></ListItem>
+            <ListItem>Output Y is Y &#x2208; &#x7B; 0,1 &#x7D; ← categorical output</ListItem>
+          </UnorderedList>
+          </UnorderedList>
           <br></br>
-          <table className="table1">
-            <tr>
-              <td><b>X:</b></td>
-              <td>set of input data</td>
-            </tr>
-            <tr>
-              <td><b>Y:</b></td>
-              <td>associated set of target values, outputs. For supervised learning</td>
-            </tr>
-            <tr>
-              <td><b>x<sup>k</sup>:</b></td>
-              <td>kth example input from a dataset</td>
-            </tr>
-            <tr>
-              <td><b>y<sup>k</sup>:</b></td>
-              <td>kth example output from a dataset</td>
-            </tr>
-            <tr>
-              <td><b>&#x211D;:</b></td>
-              <td>set of all real numbers</td>
-            </tr>
-          </table>
+          <TableContainer>
+            <Table className="table1" size='sm'>
+              <Thead>
+                <Tr>
+                  <Th>Variable</Th>
+                  <Th>Description</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td><b>X:</b></Td>
+                  <Td>set of input data</Td>
+                </Tr>
+                <Tr>
+                  <Td><b>Y:</b></Td>
+                  <Td>associated set of target values, outputs. For supervised learning</Td>
+                </Tr>
+                <Tr>
+                  <Td><b>x<sup>k</sup>:</b></Td>
+                  <Td>kth example input from a dataset</Td>
+                </Tr>
+                <Tr>
+                  <Td><b>y<sup>k</sup>:</b></Td>
+                  <Td>kth example output from a dataset</Td>
+                </Tr>
+                <Tr>
+                  <Td><b>&#x211D;:</b></Td>
+                  <Td>set of all real numbers</Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
+          
           <br></br>
-          <ul>
-          <li>But output can be more than just binary, like Y &#x2208; &#x211D;</li>
+          <UnorderedList>
+          <ListItem>But output can be more than just binary, ListItemke Y &#x2208; &#x211D;</ListItem>
           <br></br>
-          <li>Linear function <b>y=wx + b</b> where w=="weight" and b=="bias"</li>
-          <li>Need bias to have flexibility to shift slope when there is an error</li>
-          <li>Hard to do functions by hand! So use machine learning to learn a good approximation of the function from the data</li>
-        </ul>
+          <ListItem>Linear function <b>y=wx + b</b> where w=="weight" and b=="bias"</ListItem>
+          <ListItem>Need bias to have flexibiListItemty to shift slope when there is an error</ListItem>
+          <ListItem>Hard to do functions by hand! So use machine learning to learn a good approximation of the function from the data</ListItem>
+        </UnorderedList>
         <h3>"Classic" supervised learning in machine learning</h3>
         <img src="https://i.imgur.com/M5UmLq1.png" width="70%"></img>
         <h3>Testing our model on three observations</h3>
-        <ul>
-          <li>Can we do better? Maybe!</li>
-          <ul>
-            <li><mark>Option 1: collect more data and retrain</mark> ← Do this first! We need more data before we try a new function if we have a small number of observations</li>
-            <li>Option 2: try a different function</li>
-            <li>Option 3: both 1 and 2</li>
-          </ul>
+        <UnorderedList>
+          <ListItem>Can we do better? Maybe!</ListItem>
+          <UnorderedList>
+            <ListItem><mark>Option 1: collect more data and retrain</mark> ← Do this first! We need more data before we try a new function if we have a small number of observations</ListItem>
+            <ListItem>Option 2: try a different function</ListItem>
+            <ListItem>Option 3: both 1 and 2</ListItem>
+          </UnorderedList>
           <br></br>
-          <li>Learning better models - collect more data: we see we missed the actual trend. Through retraining, we can get a better capture of the data. Still see data with large errors to the linear function, though!</li>
-          <li>Introduce a polynomial: much better approximation of trends (non-linear relationship/function)</li>
-          <li>Stack linear functions with non-linear functions to capture complexity of data</li>
-        </ul>
+          <ListItem>Learning better models - collect more data: we see we missed the actual trend. Through retraining, we can get a better capture of the data. Still see data with large errors to the linear function, though!</ListItem>
+          <ListItem>Introduce a polynomial: much better approximation of trends (non-linear relationship/function)</ListItem>
+          <ListItem>Stack linear functions with non-linear functions to capture complexity of data</ListItem>
+        </UnorderedList>
+        <br></br>
         <h3>How to know which function is best? Test a new observation!</h3>
-        <table className="table1">
-            <tr>
-              <td><b>Underfit</b></td>
-              <td>“My model is not doing that well on the given data and new data”</td>
-            </tr>
-            <tr>
-              <td><b>Good Fit</b></td>
-              <td>“Doing well on given data AND new data!”</td>
-            </tr>
-            <tr>
-              <td><b>Overfit</b></td>
-              <td>“Perfect for given data but bad on new data!” (Hasn’t learned actual trends in the data!)</td>
-            </tr>
-        </table>
+        <TableContainer>
+          <Table className="table1" size="sm">
+              <Tr>
+                <Td><b>Underfit</b></Td>
+                <Td>“My model is not doing that well on the given data and new data”</Td>
+              </Tr>
+              <Tr>
+                <Td><b>Good Fit</b></Td>
+                <Td>“Doing well on given data AND new data!”</Td>
+              </Tr>
+              <Tr>
+                <Td><b>Overfit</b></Td>
+                <Td>“Perfect for given data but bad on new data!” (Hasn’t learned actual trends in the data!)</Td>
+              </Tr>
+          </Table>
+        </TableContainer>
+        <br></br>
         <h3>How to train your <strike>dragon</strike> model</h3>
-        <ul>
-          <li>Simulate scenario with data you have</li>
-        </ul>
-        <table className="table1">
-            <tr>
-              <td><b>Training Set</b></td>
-              <td>data the model <i>can</i> see</td>
-            </tr>
-            <tr>
-              <td><b>Validatons Set</b></td>
-              <td>new points the model hasn't seen yet</td>
-            </tr>
-            <tr>
-              <td><b>Test Set</b></td>
-              <td>don’t use this set at all during training process, only use it to finally evaluate work and evaluate performance</td>
-            </tr>
-        </table>
-        <ul>
-          <li><b>Training phase:</b></li>
-          <ul>
-            <li>If My model is not doing that well on training data and validation data, Then (underfit)</li>
-            <li>If My model is doing well on the training data, the performance is bad on validation data, Then (overfit)</li>
-            <li>Else my model is doing well on both!</li>
-          </ul>
-          <li><b>Test phase:</b></li>
-          <ul>
-            <li>(Ideally) I have more confidence in the generalizability of my model since we have never seen any of this data before AND the model works well!</li>
-            <li>But if it doesn’t work, what trends am I missing in the training data? What is off in my training strategy? More data could help, OR there might be other factors that need to be taken into account</li>
-          </ul>
-          <li><b>Adding features to sample:</b> x<sup>k</sup><sub>i</sub></li>
-          <ul>
-            <li>k is the index of the row</li>
-            <li>i is the index of the column (features)</li>
-          </ul>
-        </ul>
+        <UnorderedList>
+          <ListItem>Simulate scenario with data you have</ListItem>
+        </UnorderedList>
+        <TableContainer>
+          <Table className="table1" size='sm'>
+            <Tbody>
+              <Tr>
+                  <Td><b>Training Set</b></Td>
+                  <Td>data the model <i>can</i> see</Td>
+                </Tr>
+                <Tr>
+                  <Td><b>VaListItemdatons Set</b></Td>
+                  <Td>new points the model hasn't seen yet</Td>
+                </Tr>
+                <Tr>
+                  <Td><b>Test Set</b></Td>
+                  <Td>don’t use this set at all during training process, only use it to finally evaluate work and evaluate performance</Td>
+                </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+        <br></br>
+        <UnorderedList>
+          <ListItem><b>Training phase:</b></ListItem>
+          <UnorderedList>
+            <ListItem>If My model is not doing that well on training data and validation data, Then (underfit)</ListItem>
+            <ListItem>If My model is doing well on the training data, the performance is bad on validation data, Then (overfit)</ListItem>
+            <ListItem>Else my model is doing well on both!</ListItem>
+          </UnorderedList>
+          <UnorderedList><b>Test phase:</b></UnorderedList>
+          <UnorderedList>
+            <ListItem>(Ideally) I have more confidence in the generalizability of my model since we have never seen any of this data before AND the model works well!</ListItem>
+            <ListItem>But if it doesn’t work, what trends am I missing in the training data? What is off in my training strategy? More data could help, OR there might be other factors that need to be taken into account</ListItem>
+          </UnorderedList>
+          <ListItem><b>Adding features to sample:</b> x<sup>k</sup><sub>i</sub></ListItem>
+          <UnorderedList>
+            <ListItem>k is the index of the row</ListItem>
+            <ListItem>i is the index of the column (features)</ListItem>
+          </UnorderedList>
+        </UnorderedList>
+        <br></br>
         <div className="recap">
-          <h3>Recap</h3>
+          <Heading size='sm'>Recap</Heading>
           <p>How to represent inputs/outputs</p>
-          <ul>
-            <li>Represent input and output as numbers</li>
-            <li>Classification: predicting categorical outputs</li>
-            <li>Regression: predicting numerical outputs</li>
-          </ul>
+          <UnorderedList>
+            <ListItem>Represent input and output as numbers</ListItem>
+            <ListItem>Classification: predicting categorical outputs</ListItem>
+            <ListItem>Regression: predicting numerical outputs</ListItem>
+          </UnorderedList>
           <p>Supervised learning</p>
-          <ul>
-            <li>Learn a function that approximates the data well</li>
-            <li>Get more data → Try different models → Pick a good model</li>
-          </ul>
+          <UnorderedList>
+            <ListItem>Learn a function that approximates the data well</ListItem>
+            <ListItem>Get more data → Try different models → Pick a good model</ListItem>
+          </UnorderedList>
         </div>
+        </Stack>
         <br></br>
         <Return></Return>
       </div>
